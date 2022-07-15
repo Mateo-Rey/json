03f318d3b304830d4f39c3f7288ce2518a140974
Mateo Rey <mateorey@Mateos-MacBook-Air.local>
2022-07-15 13:35:40 -0400

    learning json

---
+++ testcode.js
@@ -0,0 +1,22 @@
+// let favColor = 'blue'
+// console.log(favColor);
+// let countBag = 5
+// let countShoe = 4
+// console.log(countBag + countShoe);
+// const myName = 'Mateo'
+// const sumTotal = (myName * countBag)
+// console.log(sumTotal);
+let truthisoutthere = true
+let falseisnotgood = false
+console.log(truthisoutthere + falseisnotgood);
+const firstname = 'Mateo'
+const lastname = 'Rey'
+const fullname = (firstname + lastname);
+let nameInUpperCase = fullname.toUpperCase()
+let nameInLowerCase = fullname.toLowerCase()
+console.log(nameInUpperCase, nameInLowerCase);
+let tax = 0.7
+let carWorth = 4500
+let realtax = (carWorth*tax)
+let realWorth = (carWorth + realtax)
+console.log(`my car is worth $${realWorth}`);
